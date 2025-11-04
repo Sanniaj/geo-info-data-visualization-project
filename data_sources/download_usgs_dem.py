f75394df56c03bbf52614f2ff35ebd398baa2993
Sanniaj <jsannia0130@gmail.com>
2025-11-03 19:50:15 -0800

    add initial versions of files config.py, download_nasa_firms.py , download_noaa_weather.py , download_usgs_dem.py

---
+++ data_sources/download_usgs_dem.py
@@ -0,0 +1,16 @@
+"""
+Downloads USGS Digital Elevation Model (DEM) data for California regions.
+Data source: USGS Earth Explorer API
+"""
+
+def download_dem_data(region, output_dir):
+    """Download DEM data for specified region"""
+    pass
+
+def validate_dem_file(filepath):
+    """Check if downloaded DEM file is valid"""
+    pass
+
+if __name__ == "__main__":
+    # Run as script
+    download_dem_data("california", "../data/usgs/")
